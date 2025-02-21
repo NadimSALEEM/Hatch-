b3cea018d624b28d70123eb02b798d55fe42cc32
InesAllag <81326025+InesAllag@users.noreply.github.com>
2025-02-21 11:53:37 +0100

    Add input field checks
    verification of e-mail format, password size and visibility, connection loading and deactivation of elements during connection

--- Frontend/lib/Pages/Scenario1/se_connecter.dart
+++ Frontend/lib/Pages/Scenario1/se_connecter.dart
@@ -1,9 +1,11 @@
+//Importation des packages et bibliothèques nécessaires
 import 'package:flutter/material.dart';
-import 'package:dio/dio.dart';
-import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:dio/dio.dart'; //requêtes HTTP
+import 'package:flutter_secure_storage/flutter_secure_storage.dart'; //stockage sécurisé des données
 import 'package:hatch/Pages/Scenario1/creer_un_compte.dart';
 import 'package:logger/logger.dart';
 import 'package:hatch/Pages/Scenario1/Reinit_mot_de_passe.dart';
+import 'package:email_validator/email_validator.dart'; //validation du format des emails
 
 class SeConnecter extends StatefulWidget {
   const SeConnecter({Key? key}) : super(key: key);
@@ -13,22 +15,30 @@ class SeConnecter extends StatefulWidget {
 }
 
 class _SeConnecterState extends State<SeConnecter> {
+  //Gestion des champs de saisie
   final TextEditingController _emailController = TextEditingController();
   final TextEditingController _passwordController = TextEditingController();
+
   final FlutterSecureStorage _storage = const FlutterSecureStorage();
-  final Dio _dio = Dio();
+  final Dio _dio = Dio(); //objet pour envoyer des requêtes HTTP
   final Logger _logger = Logger();
 
-  bool _loading = false;
-  bool _rememberMe = false;
-  String? _errorMessage;
+  bool _loading = false; //true si la connexion est en cours
+  bool _rememberMe =
+      false; //true si l'utilisateur veut mémoriser ses identifiants
+  String? _errorMessage; //message d'erreur en cas d'échec de connexion
+  bool _isEmailValid = false;
+  bool _isPasswordValid = false;
+  bool _obscurePassword = true; //mot de passe masqué
 
+  //Fonction pour mettre à jour l'UI en activant l'état de chargement
   Future<void> _login() async {
     setState(() {
       _loading = true;
-      _errorMessage = null;
+      _errorMessage = null; //réinitialisation du message d'erreur
     });
 
+    //Envoi de la requête HTTP
     try {
       Response response = await _dio.post(
         'http://localhost:80/auth/token',
@@ -39,6 +49,7 @@ class _SeConnecterState extends State<SeConnecter> {
         },
       );
 
+      //Traitement de la réponse
       if (response.data.containsKey('access_token')) {
         String token = response.data['access_token'];
         await _storage.write(key: 'jwt_token', value: token);
@@ -51,6 +62,7 @@ class _SeConnecterState extends State<SeConnecter> {
           Navigator.pushNamed(context, '/home');
         }
       }
+      //Gestion des erreurs
     } catch (e) {
       if (e is DioException) {
         _logger.e("🛠️ Error Response: ${e.response?.data}");
@@ -65,6 +77,7 @@ class _SeConnecterState extends State<SeConnecter> {
     }
   }
 
+  //Interface UI
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -72,8 +85,8 @@ class _SeConnecterState extends State<SeConnecter> {
         padding: const EdgeInsets.all(16.0),
         child: Column(
           crossAxisAlignment: CrossAxisAlignment.start,
-          // Logo
           children: [
+            // Logo
             Center(
               child: Image.asset(
                 'images/color_logo.png',
@@ -83,129 +96,171 @@ class _SeConnecterState extends State<SeConnecter> {
             const SizedBox(height: 20),
 
             // Champs de saisie
-            _buildEmailField(),
-            _buildPasswordField(),
-            if (_errorMessage != null)
-              Text(_errorMessage!, style: const TextStyle(color: Colors.red)),
-            const SizedBox(height: 30),
+            IgnorePointer(
+              //empêche l'interaction avec les champs de saisie pendant la connexion
+              ignoring: _loading,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  _buildEmailField(),
+                  _buildPasswordField(),
 
-            // Mémoriser identifiants / Lien "Mot de passe oublié"
-            Padding(
-              padding: const EdgeInsets.only(left: 0),
-              child: Center(
-                child: SizedBox(
-                  width: MediaQuery.of(context).size.width * 0.85,
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.start,
-                    children: [
-                      Transform.scale(
-                        scale: 0.8,
-                        child: Checkbox(
-                          value: _rememberMe,
-                          onChanged: (bool? newValue) {
-                            setState(() {
-                              _rememberMe = newValue ?? false;
-                            });
-                          },
-                          activeColor: const Color(0xFF9381FF),
-                          checkColor: Colors.white,
+                  const SizedBox(height: 15),
+                  if (_errorMessage != null)
+                    Padding(
+                      padding: EdgeInsets.only(
+                          left: MediaQuery.of(context).size.width * 0.075),
+                      child: Text(
+                        //affichage du message d'erreur s'il y a un problème de connexion
+                        _errorMessage!,
+                        style: const TextStyle(
+                          color: Colors.red,
+                          fontSize: 13,
+                          fontFamily: 'Nunito',
                         ),
                       ),
-                      Expanded(
-                        child: Text(
-                          "Mémoriser mes identifiants",
-                          style: TextStyle(
-                            fontSize: 12,
-                            fontFamily: 'Nunito',
-                            color: const Color(0xFF666666),
-                          ),
-                          textAlign: TextAlign.left,
-                        ),
-                      ),
-                      // Lien "Mot de passe oublié"
-                      TextButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            MaterialPageRoute(
-                              builder: (context) => const ReinitMotDePasse(),
+                    ),
+                  const SizedBox(height: 10),
+
+                  // Mémoriser identifiants + Mot de passe oublié
+                  Padding(
+                    padding: const EdgeInsets.only(left: 0),
+                    child: Center(
+                      child: SizedBox(
+                        width: MediaQuery.of(context).size.width * 0.85,
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.start,
+                          children: [
+                            Transform.scale(
+                              scale: 0.8,
+                              child: Checkbox(
+                                value: _rememberMe,
+                                onChanged: _loading
+                                    ? null
+                                    : (bool? newValue) {
+                                        setState(() {
+                                          _rememberMe = newValue ?? false;
+                                        });
+                                      },
+                                activeColor: const Color(0xFF9381FF),
+                                checkColor: Colors.white,
+                              ),
                             ),
-                          );
-                        },
+                            Expanded(
+                              child: Text(
+                                "Mémoriser mes identifiants",
+                                style: TextStyle(
+                                  fontSize: 12,
+                                  fontFamily: 'Nunito',
+                                  color: const Color(0xFF666666),
+                                ),
+                                textAlign: TextAlign.left,
+                              ),
+                            ),
+                            AbsorbPointer(
+                              //élément désactivé pendant le chargement
+                              absorbing: _loading,
+                              child: TextButton(
+                                onPressed: _loading
+                                    ? null
+                                    : () {
+                                        Navigator.push(
+                                          context,
+                                          MaterialPageRoute(
+                                            builder: (context) =>
+                                                const ReinitMotDePasse(),
+                                          ),
+                                        );
+                                      },
+                                child: const Text(
+                                  'Mot de passe oublié ?',
+                                  style: TextStyle(
+                                    color: Color(0xFF9381FF),
+                                    fontSize: 12,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ),
+                  ),
+
+                  const SizedBox(height: 50),
+
+                  //Bouton de connexion
+                  Center(
+                    child: _loading
+                        ? const CircularProgressIndicator() //indicateur de chargement
+                        : ElevatedButton(
+                            onPressed:
+                                (_isEmailValid && _isPasswordValid && !_loading)
+                                    ? _login
+                                    : null,
+                            style: ElevatedButton.styleFrom(
+                              padding: EdgeInsets.zero,
+                              backgroundColor: Colors.transparent,
+                            ),
+                            child: Ink(
+                              decoration: BoxDecoration(
+                                gradient: (_isEmailValid && _isPasswordValid)
+                                    ? const LinearGradient(
+                                        colors: [
+                                          Color(0xFFB9ADFF),
+                                          Color(0xFF9381FF)
+                                        ],
+                                        begin: Alignment.centerLeft,
+                                        end: Alignment.centerRight,
+                                      )
+                                    : null,
+                                borderRadius: BorderRadius.circular(8),
+                              ),
+                              child: Container(
+                                padding: const EdgeInsets.symmetric(
+                                    vertical: 15, horizontal: 130),
+                                child: Text(
+                                  'Connexion',
+                                  style: TextStyle(
+                                    fontSize: 15,
+                                    fontFamily: 'Nunito',
+                                    color: Colors.white,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          ),
+                  ),
+
+                  const SizedBox(height: 20),
+                  //Bouton création de compte
+                  Center(
+                    child: AbsorbPointer(
+                      //inactif pendant le chargement de la connexion
+                      absorbing: _loading,
+                      child: TextButton(
+                        onPressed: _loading
+                            ? null
+                            : () {
+                                Navigator.push(
+                                  context,
+                                  MaterialPageRoute(
+                                    builder: (context) => const CreerUnCompte(),
+                                  ),
+                                );
+                              },
                         child: const Text(
-                          'Mot de passe oublié ?',
+                          'Inscrivez-vous >',
                           style: TextStyle(
                             color: Color(0xFF9381FF),
-                            fontSize: 12,
+                            fontSize: 14,
+                            fontFamily: 'Nunito',
                           ),
                         ),
                       ),
-                    ],
-                  ),
-                ),
-              ),
-            ),
-
-            const SizedBox(height: 50),
-
-            // Bouton Connexion
-            Center(
-              child: ElevatedButton(
-                onPressed: () {
-                  Navigator.pushNamed(context, '/accueil');
-                },
-                style: ElevatedButton.styleFrom(
-                  padding: EdgeInsets.zero,
-                  backgroundColor: Colors.transparent,
-                ),
-                child: Ink(
-                  decoration: BoxDecoration(
-                    gradient: const LinearGradient(
-                      colors: [
-                        Color(0xFFB9ADFF),
-                        Color(0xFF9381FF),
-                      ],
-                      begin: Alignment.centerLeft,
-                      end: Alignment.centerRight,
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  child: Container(
-                    padding: const EdgeInsets.symmetric(
-                        vertical: 15, horizontal: 130),
-                    child: Text(
-                      'Connexion',
-                      style: TextStyle(
-                        fontSize: 15,
-                        fontFamily: 'Nunito',
-                        color: Colors.white,
-                      ),
                     ),
                   ),
-                ),
-              ),
-            ),
-
-            // Lien Inscrivez-vous
-            const SizedBox(height: 20),
-            Center(
-              child: TextButton(
-                onPressed: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => const CreerUnCompte(),
-                    ),
-                  );
-                },
-                child: const Text(
-                  'Inscrivez-vous >',
-                  style: TextStyle(
-                    color: Color(0xFF9381FF),
-                    fontSize: 14,
-                    fontFamily: 'Nunito',
-                  ),
-                ),
+                ],
               ),
             ),
           ],
@@ -252,10 +307,19 @@ class _SeConnecterState extends State<SeConnecter> {
                 fontSize: 16,
                 color: Color(0xFF666666),
               ),
-              decoration: const InputDecoration(
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
                 border: InputBorder.none,
-                contentPadding: EdgeInsets.symmetric(vertical: 10),
+                contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                errorText:
+                    _isEmailValid ? null : 'Email invalide', // Message d'erreur
               ),
+              onChanged: (value) {
+                setState(() {
+                  _isEmailValid = EmailValidator.validate(
+                      value); //vérification du format du mail
+                });
+              },
             ),
           ),
         ),
@@ -295,16 +359,35 @@ class _SeConnecterState extends State<SeConnecter> {
             ),
             child: TextField(
               controller: _passwordController,
-              obscureText: true,
+              obscureText: _obscurePassword,
               style: const TextStyle(
                 fontFamily: 'Nunito',
                 fontSize: 16,
                 color: Color(0xFF666666),
               ),
-              decoration: const InputDecoration(
+              decoration: InputDecoration(
                 border: InputBorder.none,
-                contentPadding: EdgeInsets.symmetric(vertical: 10),
+                contentPadding: const EdgeInsets.symmetric(vertical: 10),
+                suffixIcon: IconButton( //icône oeil pour voir le mot de passe
+                  icon: Icon(
+                    _obscurePassword ? Icons.visibility_off : Icons.visibility,
+                    color: Colors.grey,
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _obscurePassword = !_obscurePassword;
+                    });
+                  },
+                ),
+                errorText: _isPasswordValid
+                    ? null
+                    : 'Entrez au minimum 8 caractères', // Message d'erreur
               ),
+              onChanged: (value) {
+                setState(() {
+                  _isPasswordValid = value.length >= 8;
+                });
+              },
             ),
           ),
         ),
